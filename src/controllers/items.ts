55a02e68137b1978dc706e67f093ea1eed25fb68
mathi669 <matias.luarte@hotmail.com>
2023-01-20 19:41:01 -0300

    models and services

--- src/controllers/items.ts
+++ src/controllers/items.ts
@@ -1,41 +1,51 @@
 import { Request, Response } from "express"
 import { handleHttp } from "../utils/error.handle"
+import { deleteCar, getCar, getCars, insertCar, updateCar } from "../service/item.service"
 
-const getItem = (req: Request, res: Response) => {
+const getItem = async({params}: Request, res: Response) => {
     try {
-        
+        const { id } = params
+        const response = await getCar(id);
+        const data = response ? response : 'NOT_FOUND'
+        res.send(data)
     } catch (e) {
         handleHttp(res, 'Error get item')
     }
 }
 
-const getItems = (req: Request, res: Response) => {
+const getItems = async(req: Request, res: Response) => {
     try {
-        
+        const response = await getCars();
+        res.send(response)
     } catch (e) {
         handleHttp(res, 'Error get items')
     }
 }
 
-const updateItem = (req: Request, res: Response) => {
+const updateItem = async({params, body}: Request, res: Response) => {
     try {
-        
+        const {id} = params
+        const response = await updateCar(id,body);
+        res.send(response)
     } catch (e) {
         handleHttp(res, 'ERROR UPDATE ITEMS')
     }
 }
 
-const postItem = ({ body }: Request, res: Response) => {
+const postItem = async ({ body }: Request, res: Response) => {
     try {
-        res.send(body)
+        const responseItem = await insertCar(body)
+        res.send(responseItem)
     } catch (e) {
-        handleHttp(res, 'ERROR POST ITEM')
+        handleHttp(res, 'ERROR POST ITEM', e)
     }
 }
 
-const deleteItem = (req: Request, res: Response) => {
+const deleteItem = async({params}: Request, res: Response) => {
     try {
-        
+        const {id} = params
+        const response = await deleteCar(id);
+        res.send(response)
     } catch (e) {
         handleHttp(res, 'ERROR DELETE ITEM')
     }
